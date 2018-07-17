6167c362b42c5a3bbf537981f55b96d960832c63
Rom <rutcoba@gmail.com>
2018-07-17 13:58:46 +0300

    bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -49,10 +49,70 @@ $(document).ready(function () {
         })
     }
 
-    if($('#pieces').length > 0){
-        $('.js-change-svg-bg').on('click', function(){ changeBg() });
+    if ($('#pieces').length > 0) {
+        function changeBg() {
+            actualSection = (actualSection >= totalSection) ? 1 : ++actualSection;
+            $('#pieces').removeAttr('class').addClass('section' + actualSection);
+            morphingPaths(actualSection);
+        }
+        function morphingPaths(section) {
+            sections();
+            var $path1 = $('#path1'),
+                $path2 = $('#path2');
 
-        setInterval(function(){ changeBg() }, 5000);
+            switchToPath($path1, section, 'path1');
+            setTimeout(function () {
+                switchToPath($path2, section, 'path2');
+            }, 200);
+        }
+        function switchToPath($path, section, sectionPath) {
+            var pathPoints = eval('section' + section)[sectionPath];
+            var points = '';
+
+            window[sectionPath] = [];
+            actualPoints = $path.attr('points').split(' ');
+
+            for (var i = 0; i < pathPoints.length; i++) {
+                var actualPoint = actualPoints[i].split(','),
+                    obj = {
+                        x: actualPoint[0],
+                        endX: pathPoints[i].x,
+                        y: actualPoint[1],
+                        endY: pathPoints[i].y
+                    };
+                window[sectionPath].push(obj);
+            }
+
+            for (var j = 0; j < window[sectionPath].length; j++) {
+                var p = window[sectionPath][j];
+                TweenMax.to(p, 1, {
+                    x: p.endX,
+                    y: p.endY,
+                    ease: Expo.easeInOut,
+                    delay: j * .25,
+                    onUpdate: function () {
+                        animatePath($path, window[sectionPath]);
+                    }
+                }, .2);
+            }
+
+            function animatePath($path, path) {
+                var points = '';
+                for (var i = 0; i < path.length; i++) {
+                    var point = path[i].x + ',' + path[i].y;
+                    points += point + ' ';
+                }
+                $path.attr('points', points);
+            }
+        }
+
+        $('.js-change-svg-bg').on('click', function () {
+            changeBg()
+        });
+
+        setInterval(function () {
+            changeBg()
+        }, 5000);
     }
 });
 
@@ -72,114 +132,56 @@ var w = $(window).width(),
     section2,
     section3;
 
-function sections(){
+function sections() {
     w = $(window).width();
     h = $(window).height();
 
     section1 = {
         path1: [
-            { x: 0, y: 0 },
-            { x: w * 0.2, y: 0 },
-            { x: w * 0.7, y: h },
-            { x: 0, y: h }
+            {x: 0, y: 0},
+            {x: w * 0.2, y: 0},
+            {x: w * 0.7, y: h},
+            {x: 0, y: h}
         ],
         path2: [
-            { x: -w * 0.2, y: h * 0.2 },
-            { x: 0, y: 0 },
-            { x: w, y: h },
-            { x: 0, y: h }
+            {x: -w * 0.2, y: h * 0.2},
+            {x: 0, y: 0},
+            {x: w, y: h},
+            {x: 0, y: h}
         ]
     };
 
     section2 = {
         path1: [
-            { x: w * 0.15, y: 0 },
-            { x: w, y: 0 },
-            { x: w, y: h },
-            { x: w * 0.65, y: h }
+            {x: w * 0.15, y: 0},
+            {x: w, y: 0},
+            {x: w, y: h},
+            {x: w * 0.65, y: h}
         ],
         path2: [
-            { x: w, y: h * 0.3 },
-            { x: w * 2, y: h * 0.3 },
-            { x: w, y: h },
-            { x: w * 0.45, y: h }
+            {x: w, y: h * 0.3},
+            {x: w * 2, y: h * 0.3},
+            {x: w, y: h},
+            {x: w * 0.45, y: h}
         ]
     };
 
     section3 = {
         path1: [
-            { x: w * 0.1, y: 0 },
-            { x: w, y: 0 },
-            { x: w * 1.1, y: h * 0.7 },
-            { x: w, y: h * 0.95 }
+            {x: w * 0.1, y: 0},
+            {x: w, y: 0},
+            {x: w * 1.1, y: h * 0.7},
+            {x: w, y: h * 0.95}
         ],
         path2: [
-            { x: w * 0.18, y: 0 },
-            { x: w, y: 0 },
-            { x: w * 0.95, y: h },
-            { x: w * 0.55, y: h }
+            {x: w * 0.18, y: 0},
+            {x: w, y: 0},
+            {x: w * 0.95, y: h},
+            {x: w * 0.55, y: h}
         ]
     }
 }
 
-$(window).on('resize', function(){
+$(window).on('resize', function () {
     morphingPaths(actualSection);
 }).resize();
-
-function changeBg() {
-    actualSection = (actualSection >= totalSection) ? 1 : ++actualSection;
-    $('#pieces').removeAttr('class').addClass('section' + actualSection);
-    morphingPaths(actualSection);
-}
-
-function morphingPaths(section) {
-    sections();
-    var $path1 = $('#path1'),
-        $path2 = $('#path2');
-
-    switchToPath($path1, section, 'path1');
-    setTimeout(function(){
-        switchToPath($path2, section, 'path2');
-    }, 200);
-}
-
-function switchToPath($path, section, sectionPath) {
-    var pathPoints = eval('section' + section)[sectionPath];
-    var points = '';
-
-    window[sectionPath] = [];
-    actualPoints = $path.attr('points').split(' ');
-
-    for (var i = 0; i < pathPoints.length; i++) {
-        var actualPoint = actualPoints[i].split(','),
-            obj = {
-                x: actualPoint[0],
-                endX: pathPoints[i].x,
-                y: actualPoint[1],
-                endY: pathPoints[i].y
-            };
-        window[sectionPath].push(obj);
-    }
-
-    for (var j = 0; j < window[sectionPath].length; j++) {
-        var p = window[sectionPath][j];
-        TweenMax.to(p, 1, {
-            x: p.endX,
-            y: p.endY,
-            ease: Expo.easeInOut,
-            delay: j * .25,
-            onUpdate: function(){
-                animatePath($path, window[sectionPath]);
-            }
-        }, .2);
-    }
-
-    function animatePath($path, path) {
-        var points = '';
-        for (var i = 0; i < path.length; i++) {
-            var point = path[i].x + ',' + path[i].y;
-            points += point + ' ';
-        }
-        $path.attr('points', points);
-    }
-}
